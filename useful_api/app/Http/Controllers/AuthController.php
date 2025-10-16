a41dd9b07739b4c077081ceabc555d7d53afaeb2
KOUGBLENOU <kougblenousebdonan@gmail.com>
2025-10-16 15:47:08 +0100

    feat: Add logout  function in Authcontroller

--- useful_api/app/Http/Controllers/AuthController.php
+++ useful_api/app/Http/Controllers/AuthController.php
@@ -2,20 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
-    public function register (Request $request){
-       $fields = $request->validate([
-            'name'=>'required|string',
-            'email'=>'email|unique:users',
-            'password'=>'string|min:8'
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'email|unique:users',
+            'password' => 'string|min:8'
         ]);
         $user = User::create($fields);
         return $user;
     }
-     public function login (Request $request){
-        return 'login';
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'email|exists:users',
+            'password' => 'string|min:8'
+        ]);
+        $user = User::where('email', $request->email)->first();
+        // $token = $user->createToken($request->email);
+
+        // return [
+        //     'user'=> $user,
+        //     'token' => $token->plainTextToken,
+
+        // ];
+
+         if(!$user || !Hash::check($request->password, $user->password) ){
+            return[ 'message' => '401'];
+         }
+          $token = $user->createToken($request->email);
+          return [
+            'user'=> $user,
+            'token'=> $token->plainTextToken
+          ];
+
     }
 }
