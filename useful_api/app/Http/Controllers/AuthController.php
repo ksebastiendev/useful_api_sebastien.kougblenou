ef48018d3199b9b981ced499f7cc6ad6fd39d252
KOUGBLENOU <kougblenousebdonan@gmail.com>
2025-10-16 14:11:04 +0100

    feat: Add login methode in Authcontroller

--- useful_api/app/Http/Controllers/AuthController.php
+++ useful_api/app/Http/Controllers/AuthController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class AuthController extends Controller
 {
-    //
+    public function register (Request $request){
+       $fields = $request->validate([
+            'name'=>'required|string',
+            'email'=>'email|unique:users',
+            'password'=>'string|min:8'
+        ]);
+        $user = User::create($fields);
+        return $user;
+    }
+     public function login (Request $request){
+        return 'login';
+    }
 }
